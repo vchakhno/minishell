b7288ad5b3ca26d31c610289b9bcd7b0be68a35c
aubreysmie <ekhaled@student.42.fr>
2023-09-19 18:44:22 +0200

    [ADD] REPL skeleton

--- sources/main.c
+++ sources/main.c
@@ -6,20 +6,23 @@
 /*   By: ekhaled <ekhaled@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 16:13:59 by ekhaled           #+#    #+#             */
-/*   Updated: 2023/09/08 17:32:58 by ekhaled          ###   ########.fr       */
+/*   Updated: 2023/09/19 18:40:19 by ekhaled          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-bool	init_session(t_session *session)
+bool	run_repl(t_session *session)
 {
-	if (!init_history(&session->history))
-		;
-	if (!init_env(&session->env))
-		;
-	session->last_cmd_status = 0;
+	char	*line;
+	t_ast	ast;
 
+	while (!session->should_exit)
+	{
+		read_line(session, &line);//only takes one line
+		line_to_ast(line, &ast);
+		execute_ast(ast, session);
+	}
 }
 
 int	main(void)
@@ -27,6 +30,10 @@ int	main(void)
 	t_session	session;
 
 	if (!init_session(&session))
-		;
-	
+		return (EXIT_FAILURE);
+	if (!run_repl(&session))
+		return (EXIT_FAILURE);
+	if (!destroy_session(&session))
+		return (EXIT_FAILURE);
+	return (session.last_cmd_status);
 }
