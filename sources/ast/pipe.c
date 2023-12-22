1e84d35fed28ead7357295b25f84564c64a19e58
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2023-12-22 14:49:47 +0100

    Added working pipes (dirty version)

--- sources/ast/pipe.c
+++ sources/ast/pipe.c
@@ -6,11 +6,14 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 01:51:40 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/12/21 15:14:36 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/12/22 14:47:37 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
 
 bool	alloc_pipe_ast(t_pipe_ast *ast)
 {
@@ -51,17 +54,62 @@ bool	parse_pipe_ast(
 bool	execute_pipe_ast(
 	t_pipe_ast ast, t_session *session, enum e_exec_error *error
 ) {
-	t_u32	i;
+	t_u32		i;
+	pid_t		*pids;
+	int			input;
+	int			pipe_fds[2];
+	int			wstatus;
 
-	(void) session;
+	if (ast.pipes.size == 1)
+		return (!execute_cmd_ast_sync(((t_cmd_ast *)ast.pipes.elems)[0],
+			session, error));
+	if (!ft_mem_malloc(&pids, ast.pipes.size * sizeof(pid_t)))
+	{
+		*error = EXEC_ERROR_EXIT;
+		return (false);
+	}
+	input = STDIN_FILENO;
 	i = 0;
 	while (i < ast.pipes.size)
 	{
-		if (!execute_cmd_ast_sync(((t_cmd_ast *)ast.pipes.elems)[i],
-			session, error))
-			return (false);
+		if (i != ast.pipes.size - 1)
+			pipe(pipe_fds);
+		pids[i] = fork();
+		if (pids[i] == 0)
+		{
+			if (i != 0)
+			{
+				dup2(input, STDIN_FILENO);
+				close(input);
+			}
+			if (i != ast.pipes.size - 1)
+			{
+				dup2(pipe_fds[1], STDOUT_FILENO);
+				close(pipe_fds[1]);
+				close(pipe_fds[0]);
+			}
+			if (!execute_cmd_ast_async(((t_cmd_ast *)ast.pipes.elems)[i],
+				session, error))
+				return (false);
+		}
+		if (i != 0)
+			close(input);
+		close(pipe_fds[1]);
+		input = pipe_fds[0];
 		i++;
 	}
+	i = 0;
+	while (i < ast.pipes.size)
+	{
+		waitpid(pids[i], &wstatus, 0);
+		i++;
+	}
+	free(pids);
+	if (WIFEXITED(wstatus))
+		session->last_status = WEXITSTATUS(wstatus);
+	else if (WIFSIGNALED(wstatus))
+		session->last_status = 128 + WTERMSIG(wstatus);
+	ft_oprintln(ft_stderr(), "Status: {u8}", session->last_status);
 	return (true);
 }
 
