c586c4716decebe35ec6628222dee663e449bf6c
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2023-12-23 04:03:58 +0100

    Cleaned up cd and updated OLDPWD too

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 22:21:20 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/12/23 02:02:25 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/12/23 04:02:54 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool	run_builtin_cd(
-	t_vector argv, t_session *session, enum e_exec_error *error
-) {
-	char	*path;
-
-	(void) error;
-	if (argv.size == 1)
+static bool	check_args(t_u32 argc)
+{
+	if (argc == 1)
 	{
 		print_error("cd: no path argument");
-		return (true);
+		return (false);
 	}
-	if (argv.size > 2)
+	if (argc > 2)
 	{
 		print_error("cd: too many arguments");
-		return (true);
+		return (false);
 	}
-	path = ((t_string *)argv.elems)[1].c_str;
+	return (true);
+}
+
+static bool	change_dir(char *path)
+{
 	if (chdir(path) == -1)
 	{
 		print_error("cd: invalid path");
 		print_error("cd: {c_str}", strerror(errno));
-		return (true);
+		return (false);
 	}
+	return (true);
+}
+
+static bool	update_oldpwd(t_env *env)
+{
+	t_str	old_path;
+
+	if (!get_env_var(*env, ft_str("PWD"), &old_path))
+		return (true);
+	if (!set_env_var(env, ft_str("OLDPWD"), old_path))
+		return (false);
+	return (true);
+}
+
+static bool	update_pwd(t_env *env)
+{
+	char	*path;
+
 	path = getcwd(NULL, 0);
-	if (!path || !set_env_var(&session->env, ft_str("PWD"), ft_str(path)))
+	if (!path)
+		return (false);
+	if (!set_env_var(env, ft_str("PWD"), ft_str(path)))
 	{
 		free(path);
+		return (false);
+	}
+	free(path);
+	return (true);
+}
+
+bool	run_builtin_cd(
+	t_vector argv, t_session *session, enum e_exec_error *error
+) {
+	(void) error;
+	if (!check_args(argv.size))
+		return (true);
+	if (!change_dir(((t_string *)argv.elems)[1].c_str))
+		return (true);
+	if (!update_oldpwd(&session->env)
+		|| !update_pwd(&session->env))
+	{
 		*error = EXEC_ERROR_EXIT;
 		return (false);
 	}
 	ft_eprintln("cd successful!");
 	return (true);
 }
-// /media/vchakhno/velo_disk/PC/Travail/42/projects/minishell
