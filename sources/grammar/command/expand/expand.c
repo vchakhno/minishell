dfba2fa7fdd35d847e87e844ea95d7966d151b03
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2024-01-22 04:20:05 +0100

    Cleaned up expand, looks very cute now :3. Added back quotes and dquotes. Field splitting not done yet

--- sources/grammar/command/expand/expand.c
+++ sources/grammar/command/expand/expand.c
@@ -6,36 +6,76 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 08:45:57 by vchakhno          #+#    #+#             */
-/*   Updated: 2024/01/22 00:52:08 by vchakhno         ###   ########.fr       */
+/*   Updated: 2024/01/22 04:17:15 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+bool	expand_until(t_str *str, char *delim, t_fields *fields)
+{
+	t_u32	i;
+
+	i = 0;
+	while (i < str->len && !ft_str_contains_char(ft_str(delim), str->c_str[i]))
+		i++;
+	if (i && !add_field(fields, (t_str){str->c_str, i}))
+		return (false);
+	advance_str(str, i);
+	return (true);
+}
+
+bool	expand_dquotes(
+	t_str *str, t_env env, t_u8 exit_status, t_fields *fields
+) {
+	advance_str(str, 1);
+	while (str->len && *str->c_str != '"')
+	{
+		if (!expand_until(str, "\"$", fields))
+			return (false);
+		if (*str->c_str == '"')
+			break ;
+		if (!expand_var(str, env, exit_status, fields))
+			return (false);
+	}
+	advance_str(str, 1);
+	return (true);
+}
+
+bool	expand_quotes(t_str *str, t_fields *fields)
+{
+	t_u32	i;
+
+	advance_str(str, 1);
+	i = 0;
+	while (i < str->len && str->c_str[i] != '\'')
+		i++;
+	if (!add_field(fields, (t_str){str->c_str, i}))
+		return (false);
+	advance_str(str, i + 1);
+	return (true);
+}
+
 bool	expand(
 	t_str src, t_env env, t_u8 exit_status, t_vector *fields_vec
 ) {
 	t_fields	fields;
-	t_u32		i;
 
 	init_fields(&fields, fields_vec);
-	i = 0;
-	while (i < src.len)
+	while (src.len)
 	{
-		if (src.c_str[i] == '$')
-		{
-			if (i != 0 && !add_field(&fields, (t_str){src.c_str, i}))
-				return (false);
-			advance_str(&src, i);
-			i = 0;
-			if (!expand_var(&src, env, exit_status, &fields))
-				return (false);
-		}
-		else
-			i++;
+		if (!expand_until(&src, "$\"'", &fields))
+			return (false);
+		if (!src.len)
+			break ;
+		if (*src.c_str == '$' && !expand_var(&src, env, exit_status, &fields))
+			return (false);
+		else if (*src.c_str == '"'
+			&& !expand_dquotes(&src, env, exit_status, &fields))
+			return (false);
+		else if (*src.c_str == '\'' && !expand_quotes(&src, &fields))
+			return (false);
 	}
-	if (i != 0 && !add_field(&fields, (t_str){src.c_str, i}))
-		return (false);
 	return (true);
 }
 
