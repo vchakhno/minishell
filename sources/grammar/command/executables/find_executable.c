08df34e5f29fcd5853510bc51cbcd142190c64ba
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2024-01-28 11:41:16 +0100

    Added error messages for when a command is not found and rewrote find_executable to comply to the Norme

--- sources/grammar/command/executables/find_executable.c
+++ sources/grammar/command/executables/find_executable.c
@@ -6,7 +6,7 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 14:04:56 by vchakhno          #+#    #+#             */
-/*   Updated: 2024/01/28 00:27:19 by vchakhno         ###   ########.fr       */
+/*   Updated: 2024/01/28 11:39:28 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,21 @@
 
 bool	join_path(t_str path_prefix, t_str cmd_name, t_string *full_path)
 {
-	full_path->len = 0;
-	if (!ft_string_reserve(full_path, path_prefix.len + cmd_name.len + 1))
+	if (!ft_string_alloc(full_path, path_prefix.len + cmd_name.len + 2))
 		return (false);
 	ft_string_append_str(full_path, path_prefix);
 	ft_string_append_c_str(full_path, "/");
 	ft_string_append_str(full_path, cmd_name);
+	return (true);
+}
+
+bool	check_executable_at(t_str path, bool *found)
+{
+	if (access(path.c_str, F_OK) == -1)
+		return (false);
+	*found = true;
+	if (access(path.c_str, X_OK) == -1)
+		return (false);
 	return (true);
 }
 
@@ -33,28 +42,20 @@ bool	find_executable_in_path(
 	t_str_iter	path_iter;
 	bool		found;
 
-	if (!ft_string_alloc(full_path, 64))
-	{
-		*exit_status = 1;
-		return (false);
-	}
 	found = false;
 	path_iter = ft_str_split_by_c_str(path_var, ":");
 	while (path_iter.next(&path_iter, &path_prefix))
 	{
 		if (!join_path(path_prefix, cmd_name, full_path))
-		{
-			ft_string_free(*full_path);
-			*exit_status = 1;
 			return (false);
-		}
-		if (access(full_path->c_str, F_OK) == -1)
-			continue ;
-		found = true;
-		if (access(full_path->c_str, X_OK) == 0)
+		if (check_executable_at(full_path->str, &found))
 			return (true);
+		ft_string_free(*full_path);
 	}
-	ft_string_free(*full_path);
+	if (!found)
+		print_error("`{str}`: Command not found", cmd_name);
+	else
+		print_error("`{str}`: Permission denied", cmd_name);
 	*exit_status = 127 - found;
 	return (false);
 }
@@ -70,11 +71,13 @@ bool	find_executable_in_cwd(
 	if (access(full_path->c_str, F_OK) == -1)
 	{
 		*exit_status = 127;
+		print_error("`{str}`: Command not found", cmd_name);
 		return (false);
 	}
 	if (access(full_path->c_str, X_OK) == -1)
 	{
 		*exit_status = 126;
+		print_error("`{str}`: Permission denied", cmd_name);
 		return (false);
 	}
 	return (true);
@@ -89,7 +92,7 @@ bool	find_executable(
 		return (find_executable_in_cwd(cmd_name, full_path, exit_status));
 	if (!get_env_var(env, ft_str("PATH"), &path_var))
 	{
-		print_error("{str}: command not found (PATH is unset)", cmd_name);
+		print_error("{str}: PATH is unset", cmd_name);
 		*exit_status = 127;
 		return (false);
 	}
